55a0ac318e6f75b70ae1021e03ff9c6a83a7127d
caoxiaoyong <cao.xiaoyong@outlook.com>
2017-09-20 09:33:42 +0800

    9-20

---
+++ jiadewei/mini54zde/RTE/_mini54zde/RTE_Components.h
@@ -0,0 +1,26 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'mini54zde' 
+ * Target:  'mini54zde' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "Mini51Series.h"
+
+#define RTE_Drivers_ADC              /* Driver ADC */
+#define RTE_Drivers_CLK              /* Driver CLK */
+#define RTE_Drivers_GPIO              /* Driver GPIO */
+#define RTE_Drivers_I2C              /* Driver I2C */
+#define RTE_Drivers_SYS              /* Driver SYS */
+#define RTE_Drivers_Timer              /* Driver Timer */
+
+#endif /* RTE_COMPONENTS_H */
